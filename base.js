fe215fe938aa9103a03f15548dfebbc68631d3e7
novlan1 <1576271227@qq.com>
2022-05-13 10:23:42 +0800

    feat: add import plugin

--- base.js
+++ base.js
@@ -1,4 +1,11 @@
+const importSettings = require('./eslint-plugin-import-setting');
+
 module.exports = {
   parser: '@babel/eslint-parser',
-  extends: ['eslint:recommended', '@tencent/eslint-config-tencent'],
+  extends: [
+    'eslint:recommended',
+    '@tencent/eslint-config-tencent',
+    'plugin:import/recommended',
+  ],
+  settings: importSettings,
 };
--- ts.js
+++ ts.js
@@ -1,5 +1,13 @@
+const importSettings = require('./eslint-plugin-import-setting');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['@tencent/eslint-config-tencent/ts'],
+  extends: [
+    '@tencent/eslint-config-tencent/ts',
+    '@tencent/eslint-config-tencent/import',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+  ],
   plugins: ['@typescript-eslint'],
+  settings: importSettings,
 };

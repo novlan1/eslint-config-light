423b2607f7cc4e66e3eaa01d4efb8bb053215b23
novlan1 <1576271227@qq.com>
2022-09-14 17:36:21 +0800

    feat: 优化light

--- vue.js
+++ vue.js
@@ -1,4 +1,11 @@
 module.exports = {
   parser: 'vue-eslint-parser',
   extends: ['plugin:vue/recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
 };
